08dbbdec8ef1ca41c7ce19e10066609019eabe18
Koki Sakano <kosakanoki@gmail.com>
2023-06-24 11:40:47 +0900

    codeのシンタックスハイライト

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withMDX = require('@next/mdx')();
+const withMDX = require('@next/mdx')({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [
+      [
+        require('rehype-pretty-code'),
+      ],
+    ],
+  },
+});
 module.exports = withMDX(nextConfig);
